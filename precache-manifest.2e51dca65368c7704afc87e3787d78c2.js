f2b62dbc57b72db6c02f640f70b3a9dc8349c819
Tim <luckystudydanit@gmail.com>
2019-08-01 17:38:58 +0300

    Updates

---
+++ precache-manifest.2e51dca65368c7704afc87e3787d78c2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f5b17ac4096785065e964bb171e975e",
+    "url": "/monstersfinder/index.html"
+  },
+  {
+    "revision": "f9c9881e35024f2b8738",
+    "url": "/monstersfinder/static/css/main.1a9217cf.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monstersfinder/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "f9c9881e35024f2b8738",
+    "url": "/monstersfinder/static/js/main.c7553ff7.chunk.js"
+  },
+  {
+    "revision": "71ef1391c2bc28a96a8c",
+    "url": "/monstersfinder/static/js/runtime~main.bd42f7ef.js"
+  }
+]);
